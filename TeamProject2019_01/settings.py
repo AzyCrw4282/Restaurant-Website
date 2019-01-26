e99d24ee9dd462f6ccda0e68bb391bff73dc49fe
Caramel <octavio.delser@gmail.com>
2019-01-26 18:47:57 +0000

    fixed all errors and tested

--- TeamProject2019_01/settings.py
+++ TeamProject2019_01/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'MenuView.apps.MenuViewConfig',
+    'menu.apps.menuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- menu/apps.py
+++ menu/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class MenuviewConfig(AppConfig):
+class menuConfig(AppConfig):
     name = 'menu'

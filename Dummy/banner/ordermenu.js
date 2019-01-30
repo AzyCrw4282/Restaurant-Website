9af0c121cb83d0b42580766031b18a29ec13d4ed
kennethobrien <zeac119@live.rhul.ac.uk>
2019-01-30 20:18:39 +0000

    cards modified with dummy data to allow for db intergration later

---
+++ Dummy/banner/ordermenu.js
@@ -0,0 +1,22 @@
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+
+    $(function(){
+    $("#header").load("banner.html");});
+
+
+    var main  = {"mains":[
+        { "foodname":"Tacos with cereal", "price":"£5" },
+            { "foodname":"Tacos", "price":"£2" },
+            { "foodname":"Tacos with toast", "price":"£3" }]}
+
+          for (var i in main.mains){
+              var food = main.mains[i].foodname;
+              var price = main.mains[i].price;
+
+              function newCard() {
+
+
+
+              }
+
+          }

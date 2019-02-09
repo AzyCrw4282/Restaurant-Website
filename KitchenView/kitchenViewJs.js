0afe6959eed2e4ef884f058b0d2ccc655fbe34b7
Kenneth O'Brien <zeac119@live.rhul.ac.uk>
2019-02-09 19:51:01 +0000

    changes made to color scheme and data displayed making it look more presentable

--- KitchenView/kitchenViewJs.js
+++ KitchenView/kitchenViewJs.js
@@ -1,56 +1,56 @@
- var main  = {"mains":[
-                { "foodname":"Tacos with cereal", "price":"£5" },
-                { "foodname":"Tacos", "price":"£2" },
-                { "foodname":"Tacos with toast", "price":"£3" }]}
-
-        var side  = {"sides":[
-                { "sidename":"Cereal with no Tacos", "price":"£5" },
-                { "sidename":"Tacos with no Tacos", "price": "£2" },
-                { "sidename":"Toast with no Tacos", "price":"£3" }]}
-
-        var drink  = {"drinks":[
-                { "drinnkname":"Vodka", "price":"£5" },
-                { "drinkname":"Pepsi", "price":"£2" },
-                { "drinkname":"7up", "price":"£3" }]}
-
-        var deal  = {"deals":[
-                { "dealname":"The non-non-mexican", "deal":"10% off " },
-                { "dealname":"The non-mexican", "deal":"%50 off" },
-                { "dealname":"The mexican", "deal":"100% off" }]}
-
-        function newCard(id,menuitem,price) {
-            console.log("test");
-            var div = document.createElement("div");
-            //div.id = id;
-            div.className = "card";
-        div.innerHTML = "<div class='image'><h1>"+ menuitem +"</h1></div> <div class='container'> <h4>" + price + "</h4> <p><button>Cancel</p> <p><button>Done</button> </p> </div>";
-            return div;
-        }
-
-    for (var i in main.mains) {
-        var item = main.mains[i].foodname;
-        var price = main.mains[i].price;
-        var card = newCard(i,item,price);
-        document.getElementById("main").appendChild(card);
-    }
-    for (var i in side.sides) {
-        var item = side.sides[i].sidename;
-        var price = side.sides[i].price;
-        var card = newCard("side"+i,item,price);
-        document.getElementById("main").appendChild(card);
-    }
-    for (var i in drink.drinks) {
-        var item = drink.drinks[i].drinkname;
-        var price = drink.drinks[i].price;
-        var card = newCard("drinks"+i,item,price);
-        document.getElementById("main").appendChild(card);
-    }
-    for (var i in deal.deals) {
-        var item = deal.deals[i].dealname;
-        var itemdeal = deal.deals[i].deal;
-        var card = newCard("deals"+i,item,itemdeal);
-        document.getElementById("main").appendChild(card);
-    }
+ // var main  = {"orders":[
+ //                { "foodname":"Tacos with cereal", "price":"£5" },
+ //                { "foodname":"Tacos", "price":"£2" },
+ //                { "foodname":"Tacos with toast", "price":"£3" }]}
+ //
+ //        var side  = {"sides":[
+ //                { "sidename":"Cereal with no Tacos", "price":"£5" },
+ //                { "sidename":"Tacos with no Tacos", "price": "£2" },
+ //                { "sidename":"Toast with no Tacos", "price":"£3" }]}
+ //
+ //        var drink  = {"drinks":[
+ //                { "drinnkname":"Vodka", "price":"£5" },
+ //                { "drinkname":"Pepsi", "price":"£2" },
+ //                { "drinkname":"7up", "price":"£3" }]}
+ //
+ //        var deal  = {"deals":[
+ //                { "dealname":"The non-non-mexican", "deal":"10% off " },
+ //                { "dealname":"The non-mexican", "deal":"%50 off" },
+ //                { "dealname":"The mexican", "deal":"100% off" }]}
+ //
+ //        function newCard(id,menuitem,price) {
+ //            console.log("test");
+ //            var div = document.createElement("div");
+ //            //div.id = id;
+ //            div.className = "card";
+ //        div.innerHTML = "<div class='image'><h1>"+ menuitem +"</h1></div> <div class='container'> <h4>" + price + "</h4> <p><button>Cancel</p> <p><button>Done</button> </p> </div>";
+ //            return div;
+ //        }
+    //
+    // for (var i in main.mains) {
+    //     var item = main.mains[i].foodname;
+    //     var price = main.mains[i].price;
+    //     var card = newCard(i,item,price);
+    //     document.getElementById("main").appendChild(card);
+    // }
+    // for (var i in side.sides) {
+    //     var item = side.sides[i].sidename;
+    //     var price = side.sides[i].price;
+    //     var card = newCard("side"+i,item,price);
+    //     document.getElementById("main").appendChild(card);
+    // }
+    // for (var i in drink.drinks) {
+    //     var item = drink.drinks[i].drinkname;
+    //     var price = drink.drinks[i].price;
+    //     var card = newCard("drinks"+i,item,price);
+    //     document.getElementById("main").appendChild(card);
+    // }
+    // for (var i in deal.deals) {
+    //     var item = deal.deals[i].dealname;
+    //     var itemdeal = deal.deals[i].deal;
+    //     var card = newCard("deals"+i,item,itemdeal);
+    //     document.getElementById("main").appendChild(card);
+    // }
 
 
 
@@ -59,12 +59,18 @@
         if(cancel == true){
             document.getElementById('cancel').style.backgroundColor = "blue";
         }
+        if(cancel == false){
+            document.getElementById('cancel').style.backgroundColor = "#9c1816";
+        }
     }
 
      function confirmDone() {
         var done = confirm("Do you want to complete this Order");
         if(done == true){
             document.getElementById('done').style.backgroundColor = "green";
+        }
+         if(done == false){
+            document.getElementById('done').style.backgroundColor = "#9c6016";
         }
     }
 

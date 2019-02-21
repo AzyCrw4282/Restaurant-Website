ec09a1b169946ea01ff6cff6deaecc3f636b0f62
AshLimbird <aislinnlimbird@gmail.com>
2019-02-21 13:11:37 +0000

    Deleted unnecessary images

--- TeamProject2019_01/settings.py
+++ TeamProject2019_01/settings.py
@@ -83,15 +83,15 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'team1',
-        'USER': 'team1user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'POST': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3')
     }
 }
 

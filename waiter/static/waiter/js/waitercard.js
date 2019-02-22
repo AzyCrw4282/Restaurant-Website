95b883ff472e2885bf2d9ab0577f3d52b66de48e
Juuso Helvio <uni@juusoh.com>
2019-02-22 00:12:39 +0000

    Added another card format for a order cancelled from kitchen / Swapped to using the old watercard.js for clarity and ease of use

--- waiter/static/waiter/js/waitercard.js
+++ waiter/static/waiter/js/waitercard.js
@@ -88,3 +88,17 @@ function create_tag(tag_name, href, src, tag_class, id, text) {
     return tag;
 
 }
+
+function tab(evt, tabname) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabname).style.display = "block";
+    evt.currentTarget.className += " active";
+}

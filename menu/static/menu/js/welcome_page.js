77ed3bc06a0d480841d4375d01a80d29e2abaa90
zeac084 <zeac084@live.rhul.ac.uk>
2019-02-26 17:26:44 +0000

    Added better design for welcome page and enabled login redirection

--- menu/static/menu/js/welcome_page.js
+++ menu/static/menu/js/welcome_page.js
@@ -5,3 +5,13 @@ function redirect_user() {
 
     window.location+=table_code+"/"
 }
+
+/* To be used if the implemented method is not right
+function login_redirect(){
+    console.log("Redirecting to login page");
+    console.log(window.location);
+   window.location='accounts/login';
+   return false; // cancel default behavior
+
+}
+*/

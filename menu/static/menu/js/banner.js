df7228ff708b2354d2c2489ee9f1a0f236704c37
Caramel <octavio.delser@gmail.com>
2019-02-28 00:16:37 +0000

    Redid the Welcome page to a minimum standard due to very bad design, made the banner collapsible on scroll for slightly better user experience.

--- menu/static/menu/js/banner.js
+++ menu/static/menu/js/banner.js
@@ -1,15 +1,26 @@
-function banner(){
+function banner() {
     $(document).ready(function () {
-            $("a").on('click', function (event) {
-                var heightofbanner = $("#headerContainer").height() + 10;
-                console.log(heightofbanner);
-                if (this.hash !== "") {
-                    event.preventDefault();
-                    var hashlocation = this.hash;
-                    $('html, body').animate({
-                        scrollTop: $(hashlocation).offset().top - heightofbanner
-                    }, 800);
-                }
-            });
+        $("a").on('click', function (event) {
+            var heightofbanner = $("#headerContainer").height() + 10;
+            console.log(heightofbanner);
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hashlocation = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hashlocation).offset().top - heightofbanner
+                }, 800);
+            }
         });
+    });
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos < 10) {
+        document.getElementById("headerContainer").style.display = "";
+    } else if(prevScrollpos < currentScrollPos) {
+        document.getElementById("headerContainer").style.display = "None";
+    }
+    prevScrollpos = currentScrollPos;
+};

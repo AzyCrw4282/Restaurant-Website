6e6932de83e1d58c4af79671f45597745014dc4c
Caramel <octavio.delser@gmail.com>
2019-02-28 18:51:26 +0000

    fixed the redirectin issue

--- menu/urls.py
+++ menu/urls.py
@@ -9,7 +9,7 @@ from . import db_insert
 
 urlpatterns = [
                   path('', views.welcome_page, name='welcome_page'),
-                  url("r'^/",menu_redirect,name='none'),
+                  url(r'^/$',menu_redirect,name='none'),
                   url(r'^(?P<table_id>[^/]+)/$', views.menu_unsafe, name='menu_unsafe'),
                   url(r'^table_order/(?P<table_order_id>[^/]+)/$', views.menu_safe, name='menu_safe'),
 

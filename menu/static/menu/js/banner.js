70487199b4f83b909314682e4822de2a5e1068b7
Kenneth O'Brien <zeac119@live.rhul.ac.uk>
2019-03-12 14:19:20 +0000

    finished banner and hid it when user is scrolling

--- menu/static/menu/js/banner.js
+++ menu/static/menu/js/banner.js
@@ -17,11 +17,21 @@ function banner() {
 }
 
 
+$(document).scroll(function() {
+   if($(window).scrollTop() === 0) {
+     $(".banner").show();
+     $(".tabs_header").css('top', "100px");
+   }else if( !($(window).scrollTop() === 0)) {
+       $(".banner").hide();
+       $(".tabs_header").css('top', "0px");
+   }
+});
 
-window.onscroll = function () {
-    document.getElementById("banner").style.visibility = "hidden";
-    document.getElementById("tabs_header").style.top = "0px";
-}
+
+// window.onscroll = function () {
+//     document.getElementById("banner").style.visibility = "hidden";
+//     document.getElementById("tabs_header").style.top = "0px";
+// }
 
 
 

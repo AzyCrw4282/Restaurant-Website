e99ff8140e48966dc3efe9dfa15c3643444ecc19
Caramel <octavio.delser@gmail.com>
2019-03-20 19:46:05 +0000

    added chart and relevant functions to display statistics on manager's page

--- accounts/static/accounts/js/manager.js
+++ accounts/static/accounts/js/manager.js
@@ -1,10 +1,10 @@
-
 function redirect_user() {
     console.log("Order Sent");
-    var table_code=document.getElementById("table_code").value;
+    var table_code = document.getElementById("table_code").value;
 
-    window.location+=table_code+"/"
+    window.location += table_code + "/"
 }
+
 function banner() {
     $(document).ready(function () {
 
@@ -24,13 +24,61 @@ function banner() {
 }
 
 
-$(document).scroll(function() {
+$(document).scroll(function () {
     // console.log($(window).scrollTop());
-   if($(window).scrollTop() === 0) {
-     $(".banner").show();
-     $(".main_containter").css('margin-top', "100px");
-   }else if( !($(window).scrollTop() === 0)) {
-       $(".banner").hide();
-       $(".main_containter").css('margin-top', "0px");
-   }
+    if ($(window).scrollTop() === 0) {
+        $(".banner").show();
+        $(".main_containter").css('margin-top', "175px");
+        $(".tabs_header").css('top', "100px");
+    } else if (!($(window).scrollTop() === 0)) {
+        $(".banner").hide();
+        $(".main_containter").css('margin-top', "75px");
+        $(".tabs_header").css('top', "0px");
+
+    }
 });
+
+function show_stats() {
+    var container = document.getElementById("statistics_container");
+    container.style.display = "block";
+}
+
+function show_chart() {
+    var ctx = document.getElementById('order_chart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}

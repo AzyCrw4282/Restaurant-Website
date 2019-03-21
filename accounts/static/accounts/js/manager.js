2c0d0633047827883379e719d90fb9ffc96dfd23
Caramel <octavio.delser@gmail.com>
2019-03-21 16:14:10 +0000

    started making table to manage users

--- accounts/static/accounts/js/manager.js
+++ accounts/static/accounts/js/manager.js
@@ -41,10 +41,22 @@ $(document).scroll(function () {
 
     }
 });
-
+function turn_off_all_containers(){
+     var container = document.getElementById("statistics_container");
+    container.style.display = "none";
+    var accounts=document.getElementById("accounts_container");
+    accounts.style.display="none";
+}
 function show_stats() {
+    turn_off_all_containers();
     var container = document.getElementById("statistics_container");
     container.style.display = "block";
+}
+function show_accounts(){
+    turn_off_all_containers();
+    var accounts=document.getElementById("accounts_container");
+    accounts.style.display="block";
+
 }
 
 function update_profit_time_chart() {

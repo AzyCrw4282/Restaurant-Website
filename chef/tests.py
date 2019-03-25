edc993784c097fce98167d9f3b9791d8df6f45fd
zeac084 <zeac084@live.rhul.ac.uk>
2019-03-25 00:11:10 +0000

    Added comments on program execution in test files

--- chef/tests.py
+++ chef/tests.py
@@ -1,5 +1,4 @@
 
-# -*- coding: utf-8 -*-
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,23 +8,20 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-"""
-
-As a kitchen Staff I want to receive a notification to know when the customer has ordered so I can start preparing the food.
-As a kitchen Staff I want to be able to send a notification to the waiters when the order is ready so it can be given to the customer.
-
-"""
-
-
-
+#Test below will cover msot chef interactions
 class ChefNewOne(unittest.TestCase):
+
+    #This is the set-up code which adds a google chrome driver
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
+
+
     #Before running the test please ensure that waiter confirmed the order
+    #This is the main test phase for chef in which it will login and deal with order cards
     def test_chef_new_one(self):
         driver = self.driver
 
@@ -47,6 +43,8 @@ class ChefNewOne(unittest.TestCase):
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Cancel'])[1]/following::button[1]").click()
         sleep(2)
+
+    #Error handling to see if a given element exists on the page
     def is_element_present(self, how, what):
         try:
             self.driver.find_element(by=how, value=what)
@@ -54,6 +52,7 @@ class ChefNewOne(unittest.TestCase):
             return False
         return True
 
+    #If alert prompt used then this would handle it
     def is_alert_present(self):
         try:
             self.driver.switch_to_alert()
@@ -61,6 +60,7 @@ class ChefNewOne(unittest.TestCase):
             return False
         return True
 
+    #To handle any alert intractions in the test
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -73,5 +73,17 @@ class ChefNewOne(unittest.TestCase):
         finally:
             self.accept_next_alert = True
 
+#This will start the test
 if __name__ == "__main__":
     unittest.main()
+
+
+""" User stories that were covered in the test
+
+
+As a kitchen Staff I want to receive a notification to know when the customer has ordered so I can start preparing the food.
+As a kitchen Staff I want to be able to send a notification to the waiters when the order is ready so it can be given to the customer.
+
+"""
+
+
